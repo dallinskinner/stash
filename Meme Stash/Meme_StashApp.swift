082caff759656b4f0ea1e1ea61b701a461aa78d0
Dallin Skinner <dallinskinner@gmail.com>
2026-03-13 19:16:24 -0600

    Initial Commit

---
+++ Meme Stash/Meme_StashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_StashApp.swift
+//  Meme Stash
+//
+//  Created by Dallin Skinner on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct Meme_StashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Meme StashTests/Meme_StashTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meme_StashTests.swift
+//  Meme StashTests
+//
+//  Created by Dallin Skinner on 3/13/26.
+//
+
+import Testing
+@testable import Meme_Stash
+
+struct Meme_StashTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
